85c6124bbcac3d57192012216e7aeda2c0ad629c
yiyang-fairy <1196342044@qq.com>
2023-10-26 17:55:35 +0800

    滚动条优化

--- web/src/components/Page/index.tsx
+++ web/src/components/Page/index.tsx
@@ -4,7 +4,7 @@ import Board from "../Board"
 const Page = () => {
   const page = usePage()
   return (
-    <div className="flex gap-4 h-full overflow-x-scroll" id="page">
+    <div className="flex gap-4 h-full overflow-x-scroll pb-2" id="page">
       {page?.content?.map((item, index) => (
         <Board key={`${index}-${page.title}`} index={index} {...item} />
       ))}
