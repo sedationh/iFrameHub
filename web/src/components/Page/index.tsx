fc2947d88683029694e702cf0c0fcd8d60f9b504
yiyang-fairy <1196342044@qq.com>
2023-10-28 16:34:41 +0800

    滚动条优化

--- web/src/components/Page/index.tsx
+++ web/src/components/Page/index.tsx
@@ -1,10 +1,27 @@
+import { useEffect } from "react"
 import usePage from "../../hooks/usePage"
 import Board from "../Board"
 
 const Page = () => {
   const page = usePage()
+
+  useEffect(() => {
+    const page = document.getElementById("page")
+    resizeObserver.observe(page)
+  })
+
+  const resizeObserver = new ResizeObserver((entries) => {
+    for (const entry of entries) {
+      if (entry.target.clientWidth < entry.target.scrollWidth) {
+        entry.target.classList.add("pb-2")
+        return
+      }
+      entry.target.classList.remove("pb-2")
+    }
+  })
+
   return (
-    <div className="flex gap-4 h-full overflow-x-scroll pb-2" id="page">
+    <div className="flex gap-4 h-full overflow-x-auto pb-2" id="page">
       {page?.content?.map((item, index) => (
         <Board key={`${index}-${page.title}`} index={index} {...item} />
       ))}
