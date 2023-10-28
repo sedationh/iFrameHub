436da916c54187679a36fb575b7a82c9c5919b7c
yiyang-fairy <1196342044@qq.com>
2023-10-28 17:13:17 +0800

    取消监听

--- web/src/components/Page/index.tsx
+++ web/src/components/Page/index.tsx
@@ -5,12 +5,7 @@ import Board from "../Board"
 const Page = () => {
   const page = usePage()
 
-  useEffect(() => {
-    const page = document.getElementById("page")
-    resizeObserver.observe(page)
-  })
-
-  const resizeObserver = new ResizeObserver((entries) => {
+  const handleResize = (entries) => {
     for (const entry of entries) {
       if (entry.target.clientWidth < entry.target.scrollWidth) {
         entry.target.classList.add("pb-2")
@@ -18,7 +13,17 @@ const Page = () => {
       }
       entry.target.classList.remove("pb-2")
     }
-  })
+  }
+  useEffect(() => {
+    const page = document.getElementById("page")
+
+    const resizeObserver = new ResizeObserver(handleResize)
+    resizeObserver.observe(page)
+
+    return () => {
+      resizeObserver.disconnect()
+    }
+  }, [])
 
   return (
     <div className="flex gap-4 h-full overflow-x-auto pb-2" id="page">
