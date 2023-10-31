88fa7670d8f15c455b126cb3c9dcdfdefdb6eef5
yiyang-fairy <1196342044@qq.com>
2023-10-31 17:55:45 +0800

    cr

--- web/src/components/QuickJump/index.tsx
+++ web/src/components/QuickJump/index.tsx
@@ -1,65 +1,16 @@
-import { useMemo, useState } from "react"
 import { Input, Tree } from "antd"
 import { useGlobalConfig } from "../../context/globalConfig"
 import { useNavigate } from "react-router-dom"
-import { jumpBoard } from "../../utils"
+import { builTreeDataFromConfig, jumpBoard } from "../../utils"
+import { useState } from "react"
 
 const { Search } = Input
 
 export const QuickJump = ({ setOpen }) => {
-  const [expandedKeys, setExpandedKeys] = useState([])
-  const [searchValue, setSearchValue] = useState("")
-  const [autoExpandParent, setAutoExpandParent] = useState(true)
-
-  const onExpand = (newExpandedKeys: React.Key[]) => {
-    setExpandedKeys(newExpandedKeys)
-    setAutoExpandParent(false)
-  }
   const { config } = useGlobalConfig()
+  const [searchValue, setSearchValue] = useState("")
 
-  const treeData = useMemo(() => {
-    return config.map((item, index) => {
-      item.key = index + "_" + item.title
-      if (item.content) {
-        item.children = item.content.map((c) => {
-          const url = new URL(c.src)
-          const pathname = url.pathname.replace(/^(\s|\/)+|(\s|\/)+$/g, "") //去掉url.pathname首尾斜杠
-          const host = url.host.split(".").at(-2)
-          const title = pathname || host || c.src
-          return {
-            key: item.key + "_" + c.src,
-            title:
-              title.includes(searchValue) && searchValue.trim() ? (
-                <span className="bg-blue-100">{title}</span>
-              ) : (
-                <span>{title}</span>
-              ),
-          }
-        })
-      }
-      return item
-    })
-  }, [searchValue, config])
-
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target
-    const newExpandedKeys = []
-
-    treeData.forEach((item) => {
-      const parentKey = item.key
-      item.children.forEach((s) => {
-        const childrenTitle = s.title.props.children
-        if (childrenTitle.includes(value)) {
-          newExpandedKeys.push(parentKey)
-        }
-        return s
-      })
-    })
-
-    setExpandedKeys(newExpandedKeys)
-    setSearchValue(value)
-    setAutoExpandParent(true)
-  }
+  const treeData = builTreeDataFromConfig(config, searchValue.trim())
 
   const navigate = useNavigate()
 
@@ -85,15 +36,9 @@ export const QuickJump = ({ setOpen }) => {
       <Search
         style={{ marginBottom: 8 }}
         placeholder="Search"
-        onChange={onChange}
+        onChange={(e) => setSearchValue(e.target.value)}
       />
-      <Tree
-        onExpand={onExpand}
-        expandedKeys={expandedKeys}
-        autoExpandParent={autoExpandParent}
-        treeData={treeData}
-        onSelect={onSelect}
-      />
+      <Tree treeData={treeData} onSelect={onSelect} defaultExpandAll={true} />
     </div>
   )
 }
